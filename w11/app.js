3c90b2ee10e5acdac045efa15b201cca054afbc5
Raymond Bijou <raymondbijou@gmail.com>
2017-03-29 16:46:28 -0400

    satred blackjack, first versionof draw function

---
+++ w11/app.js
@@ -0,0 +1,13 @@
+const readline sync = require('readline sync');
+var deck = [
+
+];
+for (var i = 0; i < 52; i++) {
+  deck[i]
+}
+function draw() {
+  var randomcard = Math.floor (Math.random()*52);
+  if (deck[randomcard]==0) {
+return randomcard    
+  }
+}
